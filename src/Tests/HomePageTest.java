7fcaa3e9d32f470e522e262930b7cfc838373a01
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-05 20:31:15 +0530

    added code in tests to show text under hometest file

--- src/Tests/HomePageTest.java
+++ src/Tests/HomePageTest.java
@@ -5,6 +5,7 @@ public class HomePageTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is homepage testing class");
+System.out.println("after moving to Bfranch Home ,added more code in the same");
 	}
 
 }
