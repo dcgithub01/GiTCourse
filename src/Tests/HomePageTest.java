c8853919bf165e3736a3ba210fd8b94efe474b73
Deepak Chhabra <deepak.chhabra01@gmail.com>
2021-11-05 18:49:26 +0530

    added new file for testing of home page

---
+++ src/Tests/HomePageTest.java
@@ -0,0 +1,10 @@
+package Tests;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is homepage testing class");
+	}
+
+}
